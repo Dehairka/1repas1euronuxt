43ceec03ba90953b738088e6ff3d270c9125a1d5
Dehairka <esteve.charly@gmail.com>
2021-09-04 23:21:24 +0200

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="layoutDefault">
+      <div class="layoutDefault_topNav">
+          <img width="170" :src="require('@/assets/img/logo.png')" alt="logo">
+      </div>
+      <div class="layoutDefault_content">
+          <ul class="nav">
+              <li>Breakfast</li>
+              <li class="active">Lunch</li>
+              <li>Dinner</li>
+          </ul>
+          <div class="content">
+            <Nuxt />
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap');
+
+.layoutDefault{
+    background-color: $primary;
+    border-radius: 24px;
+    position: relative;
+    font-family: 'Lato', sans-serif;
+
+    &_topNav{
+        width: 100%;
+        height: 7%;
+        // background-color: grey;
+        padding: 16px;
+    }
+
+    &_content{
+        height: 85%;
+        display: flex;
+        overflow-x: visible;
+        overflow-y: hidden;
+
+        .nav{
+            margin: auto;
+            width: 10%;
+            height: 80vh;
+            display: flex;
+            justify-content: space-around;
+            flex-direction: column;
+            align-items: center;
+            position: relative;
+            li{
+                transform: rotate(270deg);
+                color: rgba(0,0,0,0.5);
+                font-weight: 600;
+                font-size: 1.3em;
+                position: relative;
+            }
+            li.active::after, li:hover::after{
+                content: ' ';
+                white-space: pre;
+                background-color: $secondary;
+                width: 5px;
+                height: 5px;
+                position: absolute;
+                padding: 5px;
+                top: 25px;
+                right: 40%;
+                border-radius: 20px;
+            }
+            li.active, li:hover{
+                color: $black;
+                cursor: pointer;
+            }
+        }
+        .content{
+            padding: 24px;
+            width: 90%;
+        }
+    }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,162 @@
+<template>
+  <div class="homePage">
+    <ul class="nav">
+      <li>All</li>
+      <li class="active">Chicken</li>
+      <li>Fish</li>
+      <li>Fruits</li>
+      <li>Pasta</li>
+      <li>Pakistani</li>
+      <li>Chinese</li>
+    </ul>
+    <div class="homePage_content">
+      <div v-for="(plat, index) in plats" :key="index" class="plat">
+      <div class="plat_background">
+        <img v-if="plat.img" class="plat_img" :src="plat.img" alt="Image plat">
+        <h2 class="plat_title">{{plat.title}}</h2>
+        <p class="plat_description">{{plat.description}}</p>
+
+      <div class="plat_ingredients">
+        <h3>Ingrédients</h3>
+        <div v-for="(ingredient, indexIng) in plat.ingredients" :key="indexIng" class="ingredient">
+          <div class="ingredient_img">
+            ok
+          </div>
+          <div class="ingredient_title">
+            {{ingredient.title}}
+          </div>
+          <div class="ingredient_quantity">
+            {{ingredient.quantity}}
+          </div>
+        </div>
+        <input class="btn" type="button" value="Voir tous les ingrédients">
+      </div>
+
+      <div class="plat_nutritionals">
+        <h3>Nutritions</h3>
+        <div v-for="(nutrition, indexNut) in plat.nutritionals" :key="indexNut" class="nutrition">
+          <div class="nutrition_title">
+            {{nutrition.title}}
+          </div>
+        </div>
+        <input class="btn" type="button" value="Recette">
+        <input class="btn--fill" type="button" value="Ajouter aux favoris">
+      </div>
+      </div>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'default',
+  data(){
+    return {
+      plats: [
+        {
+          title: "Chicken Steak",
+          description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id quibusdam, dolore totam earum eveniet accusamus ipsam autem quidem cum deleniti temporibus ducimus molestiae quae placeat. Dolores architecto odit voluptatibus impedit.",
+          img: require('@/assets/img/plats/001.png'),
+          ingredients: [
+            {
+              img: null,
+              title: "Chicken",
+              quantity: "1 Chicken Breast piece"
+            },
+            {
+              img: null,
+              title: "Chicken",
+              quantity: "1 Chicken Breast piece"
+            },
+            {
+              img: null,
+              title: "Chicken",
+              quantity: "1 Chicken Breast piece"
+            },
+            {
+              img: null,
+              title: "Chicken",
+              quantity: "1 Chicken Breast piece"
+            }
+          ],
+          nutritionals: [
+            {
+              number: "350",
+              title: "Calories",
+              value: "Kcal"
+            },
+            {
+              number: "350",
+              title: "Calories",
+              value: "Kcal"
+            },
+            {
+              number: "350",
+              title: "Calories",
+              value: "Kcal"
+            },
+            {
+              number: "350",
+              title: "Calories",
+              value: "Kcal"
+            },
+          ]
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.homePage{
+  height: 100%;
+
+  &_content{
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .nav{
+    display: flex;
+    width: 50%;
+    justify-content: space-around;
+    font-size: 1.3em;
+    font-weight: 600;
+    color: rgba(0,0,0,0.5);
+
+    .active, li:hover{
+      color: $black;
+      cursor: pointer;
+    }
+  }
+
+  .plat{
+    margin-top: 32px;
+    padding: 8px;
+    position: relative;
+    background-color: white;
+    width: 50vw;
+    border-radius: 16px;
+
+    &_img{
+      position: absolute;
+      right: -30%;
+      top: 0;
+      width: 35vw;
+      // z-index: 5;
+    }
+  }
+  .plat_background{
+    background-color: $primary;
+    border-radius: 16px;
+    padding: 24px;
+    position: relative;
+  }
+  p{
+    width: 70%;
+  }
+
+}
+</style>
